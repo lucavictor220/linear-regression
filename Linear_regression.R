e46f075897ac9c89bfbab9f11cb599a9d130eeac
Vitiok <lucavictor220@gmail.com>
2017-05-23 15:57:34 +0200

    Added create folds function, something is wrong with splitting...

--- Linear_regression.R
+++ Linear_regression.R
@@ -1,14 +1,20 @@
 rm(list=ls())
 
 library("caret")
-set.seed(3456)
 setwd("/Users/vitiok/University/IS/assignment_2")
 
 nrOfFolds <- 5
 
 data <- read.csv(file="hormigon.csv", header=TRUE, sep=",")
-folds <- createMultiFolds(data, nrOfFolds, 1)
 
+trainSet <- createMultiFolds(y=data, k=nrOfFolds, 1)
+validateSet <- createFolds(y=data, k=nrOfFolds, list=TRUE, returnTrain = TRUE)
 
+trainSet
 
+validateSet
+
+createFol
+
+training.data = data[1, ]
 
