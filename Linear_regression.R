2e334b8e865d49ab99cd4e89a410ed0ae69f85a1
Vitiok <lucavictor220@gmail.com>
2017-05-23 18:41:34 +0200

    Work in progress. Working with only one fold from createMultiFolds() function. Test indexes found.

--- Linear_regression.R
+++ Linear_regression.R
@@ -5,19 +5,28 @@ setwd("/Users/vitiok/University/IS/assignment_2")
 
 nrOfFolds <- 5
 
+# read data from csv file
 data <- read.csv(file="hormigon.csv", header=TRUE, sep=",")
 
-trainSet <- createMultiFolds(y=data[, 1], k=nrOfFolds, 1)
-validateSet <- createFolds(y=data[, 1], k=nrOfFolds, list=TRUE, returnTrain = TRUE)
+# create k-forlds cross-validation. createMultiFolds() returns folds with vector with indexes of traing data
+partitions <- createMultiFolds(y=data[, 1], k=nrOfFolds, 1)
 
 
-partitionedData <- createDataPartition(y=data[, 1], times=1, p=0.5, list=TRUE, groups=min(5, length(data)))
-
-trainSet
-
-validateSet
-
-createFol
+# Start ====== Uncomment to use another function which create training data ================= 
 
 
+#trainingData <- createFolds(y=data[, 1], k=nrOfFolds, list=TRUE, returnTrain = TRUE)
+#partitions <- createDataPartition(y=data[, 1], times=1, p=0.5, list=TRUE, groups = min(5, length(data[, 1])))
+
+# ========================================== end
+
+
+# create a vector with all the indexes of the training data by taking data from the fold1 of partitions variable
+trainingData1 <- partitions$Fold1.Rep1;
+
+# create a vector with all indexes of data
+indexesOfInitialData <- c(1:length(data[, 1]))
+
+# find indexes of test data for the first data frame taken from createMultiFolds()
+testData1 <- indexesOfInitialData[is.na(pmatch(indexesOfInitialData, trainingData1))]
 
