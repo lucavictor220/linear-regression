1e9a2674b901549ad8fa01d0126cb220c38a6a9e
Vitiok <lucavictor220@gmail.com>
2017-05-23 17:31:42 +0200

    Added splitting. I use first column as parameter vector for splitting function

--- Linear_regression.R
+++ Linear_regression.R
@@ -7,8 +7,11 @@ nrOfFolds <- 5
 
 data <- read.csv(file="hormigon.csv", header=TRUE, sep=",")
 
-trainSet <- createMultiFolds(y=data, k=nrOfFolds, 1)
-validateSet <- createFolds(y=data, k=nrOfFolds, list=TRUE, returnTrain = TRUE)
+trainSet <- createMultiFolds(y=data[, 1], k=nrOfFolds, 1)
+validateSet <- createFolds(y=data[, 1], k=nrOfFolds, list=TRUE, returnTrain = TRUE)
+
+
+partitionedData <- createDataPartition(y=data[, 1], times=1, p=0.5, list=TRUE, groups=min(5, length(data)))
 
 trainSet
 
@@ -16,5 +19,5 @@ validateSet
 
 createFol
 
-training.data = data[1, ]
+
 
