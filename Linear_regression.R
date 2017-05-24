cdb515af4170c49df2d938d782696eed9c6363f2
Vitiok <lucavictor220@gmail.com>
2017-05-24 14:29:54 +0200

    Added creating of linear model. Can't understand why should I remove column corresponding to the one we want to make prediction on.

--- Linear_regression.R
+++ Linear_regression.R
@@ -36,6 +36,24 @@ trainingData_1 <- data[trainingData_1_Indexes, ];
 # Extract test data from the indexes
 testData_1 <- data[testData_1_Indexes, ];
 
+# Find variables needed for creating linear model
+factors <- c(names(data));
+
+# This variable holds the column name we want to create model upon on 
+attributeToWorkWith <- "strength";
+
+# Remove strength column... To avoid worning with lm() 
+factors <- factors[factors != attributeToWorkWith];
+
+
+# Generate formula from the given factors
+linearModelFormula <- as.formula(paste("strength ~", paste(factors, collapse="+")));
+
+# Create linear model from the formula and data given as data.frame
+linearModel <- lm(linearModelFormula, trainingData_1);
+
+linearModel
+
 # Check for length of columns to prove everything is working properly
 length(trainingData_1[, 1])
 length(testData_1[, 1])
